5111cb57fcde4260167d9d33d392ffe9602fc484
Tlinus <txl@hotmail.fr>
2017-06-03 04:28:07 +0200

    Arena preparation et controller

--- app/controllers/arena_controller.rb
+++ app/controllers/arena_controller.rb
@@ -4,9 +4,17 @@ class ArenaController < ApplicationController
 		@welcome = "Bienvenue sur Arena, Arène de combat pour tous les aventuriers!"
 	end
 
-	def Arena
+	def new
 		@persos = Perso.all()
 		@equip = Equipement.all()
 		@avatar = Avatar.all()
+	end
+
+	def fight
+		@perso1 = Perso.find(params[:arena][:perso1_id])
+		@perso2 = Perso.find(params[:arena][:perso2_id])
+
+		@avatarPerso1 = Avatar.find(@perso1.avatar_id)
+		@avatarPerso2 = Avatar.find(@perso2.avatar_id)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'arena#index', as: 'home'
   get 'Arena' => 'arena#new'
+  post 'Arena/' => 'arena#fight'
+
 
   resources :stuffs
   resources :persos
